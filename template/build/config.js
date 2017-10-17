ca411310316d39d5ac93617911b193083593da8e
zhangrui20 <zhangrui20@baidu.com>
2017-10-17 17:26:30 +0800

    modify

--- template/build/config.js
+++ template/build/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     dev: {
         outputPath: path.resolve(__dirname, './static'),// 对应output中的path
-        outputPublicPath: 'http://www.hao123.com/',// 对应output中的publicPath,
+        outputPublicPath: '{{site}}'.match(/(\w+:\/\/)?(.*?\/)/)[0],// 对应output中的publicPath,
         outputSubDirectory: 'resource/{{name}}/',// 资源的二级目录
         port: 3000
     },
